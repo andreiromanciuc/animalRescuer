5eb57e8a5b821424c89e76bfc3cb32e8cb52cdac
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2019-12-05 14:32:49 +0200

    updates

--- src/main/java/org/fasttrackit/Dog.java
+++ src/main/java/org/fasttrackit/Dog.java
@@ -2,9 +2,10 @@ package org.fasttrackit;
 
 public class Dog extends Animal {
 
-
-    public void highSpirit(Animal animal){
-        System.out.println(animal.getName()+" wags his tail");
+    @Override
+    public void highSpirit() {
+        super.highSpirit();
+        System.out.println(getName() + " wags his tail.");
     }
 
     public Dog(String name) {
@@ -14,10 +15,5 @@ public class Dog extends Animal {
     public Dog() {
         this.setName(getName());
     }
-
-
-
-
-
 
 }
